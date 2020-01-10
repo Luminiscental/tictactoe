46c5cf307c1d5773dfb4dd87528528def129bb7e
Luminiscental <luminiscental01@gmail.com>
2020-01-10 14:47:54 +0000

    Use a deps.edn in tictactoe-clj

--- tictactoe-clj/project.clj
+++ tictactoe-clj/project.clj
@@ -1,9 +1,23 @@
+(require '[clojure.edn :as edn])
+
+(def +deps+ (-> "deps.edn" slurp edn/read-string))
+
+(defn deps->vec [deps]
+  (keep (fn [[dep {:keys [:mvn/version exclusions]}]]
+          (when version
+            (cond-> [dep version]
+              exclusions (conj :exclusions exclusions))))
+        deps))
+
+(def dependencies
+  (deps->vec (:deps +deps+)))
+
 (defproject tictactoe-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "CLI tic-tac-toe implementation"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies ~dependencies
   :main ^:skip-aot tictactoe-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
